ea31b5e8cdb3448679e3b033e51402b65a526011
Mariam Ashraf <49536384+mariemashrafkamel@users.noreply.github.com>
2021-05-11 18:48:08 +0200

    updated

--- README.md
+++ README.md
@@ -6,8 +6,6 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+I modified the starter project and converted it from static to dynamic using HTML, CSS and Javascript
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- js/app.js
+++ js/app.js
@@ -11,19 +11,19 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -31,7 +31,7 @@
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 
@@ -46,12 +46,62 @@
  * End Main Functions
  * Begin Events
  * 
-*/
-
+ * <li>
+                    <a href="#section1">Section 1</a>
+                </li>
+                <li><a href="#section2">Section 2</a></li>
+                <li><a href="#section3">Section 3</a></li>
+                <li><a href="#section4">Section 4</a></li>
+                <li><a href="#section5">Section 5</a></li>
+ */
+var active = document.getElementsByTagName("section");
+var secs = document.getElementsByClassName("sec");
 // Build menu 
+var ull = document.getElementById("navbar__list");
 
+
+
+for (var i = 1; i < 6; i++) {
+    var tag = document.createElement("li");
+    var tag_a = document.createElement("a");
+
+    tag_a.href = "#section" + i;
+    tag_a.innerText = "section" + i;
+
+    tag.appendChild(tag_a);
+    ull.appendChild(tag);
+
+}
 // Scroll to section on link click
 
 // Set sections as active
 
 
+
+for (var i = 0; i < secs.length; i++) {
+    secs[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("your-active-class");
+        current[0].className = current[0].className.replace(" your-active-class", "");
+        this.className += " your-active-class";
+    });
+}
+
+var top = document.getElementById("top");
+window.onscroll = function() { scroll_down() };
+
+function scroll_down() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        top.style.display = "block";
+    } else {
+        top.style.display = "none";
+    }
+}
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function scrollWin() {
+    window.scrollTo(0, 0);
+}
