38d75519398a77082d7474cc74e5146d3ab82f26
Mariam Ashraf <49536384+mariemashrafkamel@users.noreply.github.com>
2021-05-14 15:57:30 +0200

    active section updated

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
  * 
  */
 
-
 /**
  * End Global Variables
  * Start Helper Functions
@@ -22,22 +21,47 @@ let ull = document.getElementById("navbar__list");
 for (let i = 1; i < 6; i++) {
     let tag = document.createElement("li");
     let tag_a = document.createElement("a");
-
-    tag_a.href = "#section" + i + ' ';
-    /*tag_a.addEventListener("click", function() {
-    //document.getElementByClassName("active").style.color = "white";
-    console.log("hello");
-}); */
+    let list_id = "nav-list" + i;
+    //add id=section i and class=section i
+    //tag.setAttribute("id", list_id);
+    tag.setAttribute("class", list_id);
+    tag_a.href = "#section" + i;
     tag_a.innerText = "section " + i;
 
     tag.appendChild(tag_a);
-    tag.classList.add("not-active")
     ull.appendChild(tag);
-    tag_a.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    let sec_target = document.getElementById("section" + i);
+    let li_target = document.getElementById(list_id);
+
 }
 
-// Add class 'active' to section when near top of viewport
+let lii = document.getElementsByTagName('li');
 
+
+// Add class 'active' to section when it is near top of viewport
+function makeActive() {
+    for (let i = 0; i < 5; i++) {
+        const box = secs[i].getBoundingClientRect();
+        // You can play with the values in the "if" condition to further make it more accurate. 
+        if (box.top <= 150 && box.bottom >= 150) {
+            // Apply active state on the current section and the corresponding Nav link.
+            secs[i].classList.add("your-active-class");
+            lii[i].classList.add("active");
+
+        } else {
+            // Remove active state from other section and corresponding Nav link.
+            secs[i].classList.remove("your-active-class");
+            lii[i].classList.remove("active");
+        }
+        // console.log(i);
+    }
+}
+
+// Make sections active
+document.addEventListener("scroll", function() {
+    makeActive();
+});
+/*
 let lii = document.getElementsByTagName('li');
 
 lii[0].classList.add("active");
@@ -51,10 +75,29 @@ for (let i = 0; i < lii.length; i++) {
 
     });
 }
-
+let section = document.querySelectorAll('section');
+let a_href = document.getElementsByTagName('a');
+// Add class 'active' to section when it is near top of viewport
+function makeActive() {
+    for (const sec of secs) {
+        const box = sec.getBoundingClientRect();
+        // You can play with the values in the "if" condition to further make it more accurate. 
+        if (box.top <= 150 && box.bottom >= 150) {
+            // Apply active state on the current section and the corresponding Nav link.
+            // a_href.classList.add('active');
+            console.log(box);
+        } else {
+            // Remove active state from other section and corresponding Nav link.
+            console.log(box);
+        }
+    }
+}
 // Scroll to section on link click
 
-// Set sections as active
+
+document.addEventListener("scroll", function() {
+    makeActive();
+});
 
 
 
@@ -63,15 +106,12 @@ for (let i = 0; i < secs.length; i++) {
         let current = document.getElementsByClassName("your-active-class");
         current[0].className = current[0].className.replace(" your-active-class", "");
         this.className += " your-active-class";
-        secs[i].scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-
+        //secs[i].scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
     });
-
-    //secs[i].style.background = "none";
-
 }
 
+
+*/
 //var of scroll to top button
 let scrollToTopBtn = document.getElementById("scrollToTopBtn")
 let rootElement = document.documentElement
@@ -83,9 +123,11 @@ function scrollToTop() {
         behavior: "smooth"
     })
 }
-scrollToTopBtn.addEventListener("click", scrollToTop)
+
 
 //hide nav bar while scrolling down
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
